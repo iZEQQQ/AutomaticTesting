58f9454fcbf9223cf7321c1acc7954963e6818f8
jgorny <iceziro@wp.pl>
2021-04-12 20:49:34 +0200

    feat:  added 4 new methods to calculator test: added new tests

--- lab3/src/main/java/edu/pjatk/jgorny/Calculator.java
+++ lab3/src/main/java/edu/pjatk/jgorny/Calculator.java
@@ -1,5 +1,6 @@
 package edu.pjatk.jgorny;
 
+
 public class Calculator {
 
     public double add(double x, double y) {
@@ -29,10 +30,29 @@ public class Calculator {
         }
     }
 
+    public String addingStrings(String one, String two) {
+        return one + two;
+    }
 
+    public boolean isEven(int number) {
+        return (number % 2) != 1;
+    }
 
+    public int timesThree(int number) {
+        return number * 3;
+    }
 
+    public int times10(int number) {
+        return number * 10;
+    }
 
+    public double sqrt(double number){
+        return Math.sqrt(number);
+    }
+
+    public int timesNumber(int number) {
+        return number * number * number;
+    }
 
 
 }
---
+++ lab3/src/test/java/edu/pjatk/jgorny/CalculatorTest.java
@@ -0,0 +1,222 @@
+package edu.pjatk.jgorny;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+
+    @Test
+    public void add_correctInput_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.add(4, 6);
+
+//        Assert
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void add_doubleInput_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.add(4.5, 6.5);
+
+//        Assert
+        assertEquals(11, result);
+    }
+
+
+    @Test
+    public void div_divisionBy0_exception() {
+//        Prepare
+        Calculator cal = new Calculator();
+        try {
+//        Execute
+            double result = cal.div(4.5, 0);
+            fail("Should thrown exception");
+        } catch (IllegalArgumentException ex) {
+//        Assert
+            assertEquals("Division by 0", ex.getMessage());
+        }
+    }
+
+
+    @Test
+    public void div_dividing2by3_nearCorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.div(2, 3);
+
+//        Assert
+        assertEquals(0.67, result, 0.01);
+    }
+
+    @Test
+    public void sub_correctInput_CorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.sub(2, 3);
+
+//        Assert
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void isPositive_positiveInput_CorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        boolean result = cal.isPositive(3);
+
+//        Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void isPositive_negativeInput_CorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        boolean result = cal.isPositive(-3);
+
+//        Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void mult_correctInput_CorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.mult(4, 4);
+
+//        Assert
+        assertEquals(16, result);
+    }
+
+
+    @Test
+    public void mult_multiplyingBy0_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.mult(4, 0);
+
+//        Assert
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void mult_multiplyingByNegative_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.mult(4, -4);
+
+//        Assert
+        assertEquals(-16, result);
+    }
+
+
+    @Test
+    public void addingStrings_addingCorrectString_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        String result = cal.addingStrings("cyk", "tak");
+
+//        Assert
+        assertEquals("cyktak", result);
+    }
+
+    @Test
+    public void isEven_checkingIs10Even_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        Boolean result = cal.isEven(10);
+
+//        Assert
+        assertEquals(true, result);
+    }
+
+
+    @Test
+    public void isEven_checkingIs1Even_uncorrectResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        Boolean result = cal.isEven(1);
+
+//        Assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void timesThree_times3is9_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        int result = cal.timesThree(3);
+
+//        Assert
+        assertEquals(9, result);
+    }
+
+    @Test
+    public void times10_times10is90_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        int result = cal.times10(9);
+
+//        Assert
+        assertEquals(90, result);
+    }
+
+    @Test
+    public void sqrt_sqrtOf4_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.sqrt(4);
+
+//        Assert
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void timesNumber_3x3_correctResult() {
+//        Prepare
+        Calculator cal = new Calculator();
+
+//        Execute
+        double result = cal.timesNumber(3);
+
+//        Assert
+        assertEquals(27, result);
+    }
+
+
+}
